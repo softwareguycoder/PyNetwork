be82a33f3729b060b852a6620cb118422142f25b
Brian Hart <brian.hart@xylogix.net>
2018-10-19 08:33:42 -0400

    19 Oct 2018 Added logging messages to the server

--- Server/Server.py
+++ Server/Server.py
@@ -2,8 +2,12 @@
 
 import socket
 
+print("[*] S: Creating new TCP endpoint...")
+
 # create a server socket
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+print("[*] S: Getting local machine IP address...")
 
 # Get the IP address of the server
 ip = socket.gethostbyname(socket.gethostname())
@@ -11,27 +15,28 @@ ip = socket.gethostbyname(socket.gethostname())
 # port (user-defined)
 port = 9000
 
+print("[*] S: Configured to listen on port", port)
+
 address = (ip,port)
+
+print("[*] S: Binding TCP endpoint to", ip, " on port", port, "...")
 
 server.bind(address)
 
 server.listen(128)
 
-print("[*] Started listening on ", ip, "and", port)
+print("[*] S: Started listening on ", ip, "and", port, "...")
 
 client, addr = server.accept()
 
-print("[*] Got a connection from ", addr[0], "on port", addr[1])
+print("[*] S: Got a connection from ", addr[0], "on port", addr[1])
 
 while True:
     data = client.recv(1024)
     print("[*] C:", data)
 
-    print("[*] Processing the data...")
-    if (data=="Hello server"):
-        client.send("Hello client")
-        print("[*] S: Hello client")
-    elif(data=="QUIT"):
+    print("[*] S: Processing the data...")
+    if(data=="QUIT"):
         client.send("Goodbye!")
         print("[*] S: Goodbye!")
         print("[*] S: <disconnected>")
@@ -42,5 +47,8 @@ while True:
         print("[*] S:", data)
         client.send(data)
 
+print("[*] S: Closing endpoint...")
+
 server.close()
-print("[*] Server: done.")
+
+print("[*] S: done.")
